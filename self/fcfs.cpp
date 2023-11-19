d92b8b57c9c74c389c469312711ee1abdb3f074f
L0STT69 <151459067+L0STT69@users.noreply.github.com>
2023-11-19 23:42:32 +0530

    fcfss.cpp

--- self/fcfs.cpp
+++ self/fcfs.cpp
@@ -1,37 +1,40 @@
 #include <iostream>
 using namespace std;
 
-void findwaitingtime(int processes[], int n, int bt[], int wt[])
+void findwaitingtime(int processes[], int n, int bt[], int wt[], int at[])
 {
     wt[0] = 0;
    
     for (int i = 1; i < n; i++)
     {
-        wt[i] = bt[i - 1] + wt[i - 1];
+        wt[i] = bt[i - 1] + wt[i - 1] - at[i];
+        // Ensure waiting time is not negative
+        if (wt[i] < 0) {
+            wt[i] = 0;
+        }
     }
 }
 
 void findturnaroundtime(int processes[], int n, int bt[], int wt[], int tat[])
 {
-    for (int i = 1; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         tat[i] = bt[i] + wt[i];
     }
 }
 
-void findavgtime(int processes[], int n, int bt[])
+void findavgtime(int processes[], int n, int bt[], int at[])
 {
     int wt[n], tat[n];
     int total_wt = 0;
     int total_tat = 0;
 
-    
-    findwaitingtime(processes, n, bt, wt);
+    findwaitingtime(processes, n, bt, wt, at);
 
-    
     findturnaroundtime(processes, n, bt, wt, tat);
   
     cout << "processes  "
+         << "arrival_time  "
          << "burst_time  "
          << "Waiting_time "
          << " turn_around_time\n"<<endl;
@@ -40,11 +43,11 @@ void findavgtime(int processes[], int n, int bt[])
     {
         total_wt = total_wt + wt[i];
         total_tat = total_tat + tat[i];
-       cout << "   " << i+1 << "\t\t" << bt[i] <<"\t    "
+        cout << "   " << i+1 << "\t\t" << at[i] << "\t\t" << bt[i] <<"\t    "
             << wt[i] <<"\t\t  " << tat[i] <<endl;
         
     }
-     cout << "Average waiting time = "
+    cout << "Average waiting time = "
          << (float)total_wt / (float)n;
     cout << "\nAverage turn around time = "
          << (float)total_tat / (float)n;
@@ -54,7 +57,8 @@ int main()
 {
     int processes[] = {1, 2, 3};
     int burst_t[] = {10, 5, 8};
+    int arrival_t[] = {0, 2, 4}; // Example arrival times
     int n = sizeof(processes) / sizeof(processes[0]);
-    findavgtime(processes, n, burst_t);
+    findavgtime(processes, n, burst_t, arrival_t);
     return 0;
 }
